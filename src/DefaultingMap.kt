2c663e5081058380b723ddda28d5625d8e34bb40
Steve Gravrock <sdg@panix.com>
2018-12-09 10:54:09 -0800

    Combined into a single source file

--- src/DefaultingMap.kt
+++ src/DefaultingMap.kt
@@ -12,3 +12,8 @@ class DefaultingMap<K, V> {
         storage[key] = value
     }
 }
+
+fun crash() {
+    val m = DefaultingMap<Int, Long>(0)
+    m[0] += 42L
+}
--- src/main.kt
+++
@@ -1,4 +0,0 @@
-fun crash() {
-    val m = DefaultingMap<Int, Long>(0)
-    m[0] += 42L
-}
